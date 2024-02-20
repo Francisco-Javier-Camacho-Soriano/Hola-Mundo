39a4aeee153dadaadf741bfe51ff5688bb282cc2
Francisco-Javier-Camacho-Soriano <159966420+Francisco-Javier-Camacho-Soriano@users.noreply.github.com>
2024-02-20 00:20:22 +0000

    Version 1

---
+++ include/Alimento.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+class Alimento
+{
+    private:
+    int Energia;
+
+    public:
+    Alimento(int Energia){
+        this ->Energia = Energia;
+    }
+    int ExtraerEnergia(){
+        return this ->Energia;
+    }
+    ~Alimento(){}
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <mascota.hpp>
+#include <Alimento.hpp>
+
 using namespace std;
+
 int main()
 {
-    cout<<"Hola_"<<"23110165"<<endl;
+    Mascota m1;
+    Mascota m2;
+
+    Alimento a1(5);
+    Alimento a2(2);
+
+    m1.comer(a1);
+    m2.comer(a2);
+
+    cout << "Mascota 1 Energia:"<< m1.LeerEnergia() << endl
+    <<"Mascota 2 Energia:" << m2.LeerEnergia()<<endl;
+
     return 0;
 }
